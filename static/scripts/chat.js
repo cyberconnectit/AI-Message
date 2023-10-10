ba59b2d398a6a904579c1e53b4cd09191027f87b
Cyber Connect IT <100221210+cyberconnectit@users.noreply.github.com>
2023-10-10 11:55:06 +0100

    Update chat.js

--- static/scripts/chat.js
+++ static/scripts/chat.js
@@ -35,7 +35,15 @@ function getTime() {
 
 // Gets the first message
 function firstBotMessage() {
-    let firstMessage = "Hi, Welcome to your virtual AI IT Assistant"
+    let firstMessage = "Hello! 👋️ 
+I'm Fred the Cyber Connect IT Bot and I'm here to try to answer your questions,
+    
+How can I help you today
+    
+ To check what services Cyber Connect IT does check out the link:
+ https://cyberconnectit.github.io/main-website/services" 
+
+    
     document.getElementById("botStarterMessage").innerHTML = '<p class="botText"><span>' + firstMessage + '</span></p>';
 
     let time = getTime();
